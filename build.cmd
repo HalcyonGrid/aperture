c99887087d0aa92015601601ae7a3bd6fad53762
Ricky C <kf6kjg@users.noreply.github.com>
2018-08-26 21:07:21 -0700

    Conversion to same build tooling as whip-server
    Makes things similar and a lot less work and weirdness.

---
+++ build.cmd
@@ -0,0 +1,17 @@
+IF NOT DEFINED 1 (
+	SET buildType=Debug
+) ELSE (
+	SET buildType=%1
+)
+
+REM Build
+conan install . -s build_type=%buildType% -s arch=x86_64 -s compiler.runtime=MTd --build=missing
+MKDIR build
+CD build
+cmake -G "Visual Studio 15 2017 Win64" .. -DCMAKE_BUILD_TYPE=%buildType%
+cmake --build .
+CD ..
+
+REM Prepare package
+MKDIR bin
+COPY build\bin\aperture.exe bin
